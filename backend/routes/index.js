c96a324ce2aa54153ce57ef54b702938103b8260
Alexander Vitchinov <vitalex92@yandex.ru>
2021-03-03 20:45:51 +0300

    passed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,12 +7,6 @@ const registerValidation = require('../middlewares/validators/register');
 const loginValidation = require('../middlewares/validators/login');
 const { NotFound } = require('../errors');
 
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 router.post('/signup', registerValidation, createUser);
 router.post('/signin', loginValidation, login);
 
