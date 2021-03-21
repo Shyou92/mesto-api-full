0e7ea00673341c2ab0c97a1813b499c4ee27f649
Alexander Vitchinov <vitalex92@yandex.ru>
2021-03-21 23:23:11 +0300

    port

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3002 } = process.env;
 
 const app = express();
 
